b22f2e3a493eab20d4e1b859cd5d28c0efed0df3
Magewirephp <91219612+magewirephp@users.noreply.github.com>
2021-10-13 09:12:02 +0200

    Develop (#4)
    * Update composer.json name value
    
    * [develop]:+ New examples
    
    * [develop]:+ Readme improvements
    
    * [develop]:+ Minor UI & documentation tweaks
    
    * [develop]:+ New examples
    
    Co-authored-by: Willem Poortman <68-willempoortman@users.noreply.gitlab.hyva.io>

---
+++ src/Magewire/Reacticon.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © Willem Poortman 2021-present. All rights reserved.
+ *
+ * Please read the README and LICENSE files for more
+ * details on copyrights and license information.
+ */
+
+namespace Magewirephp\MagewireExamples\Magewire;
+
+use Magewirephp\Magewire\Component;
+use Magewirephp\Magewire\Model\Element\FlashMessage;
+
+/**
+ * @method int getQuantity()
+ * @method bool hasQuantity()
+ * @method int getInterval()
+ * @method bool hasInterval()
+ * @method bool getIncrement()
+ * @method bool hasIncrement()
+ * @method bool getDecrement()
+ * @method bool hasDecrement()
+ */
+class Reacticon extends Component
+{
+    public $quantity  = 1;
+    public $interval  = 1;
+    public $increment = true;
+    public $decrement = true;
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public function updatingInterval(string $value): string
+    {
+        if ((int) $value === 10) {
+            $this->emitTo('magewire.dispatch-events', 'rick:roll');
+        }
+        return $value;
+    }
+
+    /**
+     * @param bool $value
+     * @return bool
+     */
+    public function updatingIncrement(bool $value): bool
+    {
+        $this->dispatchBrowserEvent('toggle-button', ['increment' => $value]);
+        return $value;
+    }
+
+    /**
+     * @param bool $value
+     * @return bool
+     */
+    public function updatingDecrement(bool $value): bool
+    {
+        $this->dispatchBrowserEvent('toggle-button', ['decrement' => $value]);
+        return $value;
+    }
+
+    /**
+     * @return void
+     */
+    public function increment(): void
+    {
+        $this->quantity = $this->getQuantity() + $this->getInterval();
+    }
+
+    /**
+     * @return FlashMessage|void
+     */
+    public function decrement()
+    {
+        $calc = $this->getQuantity() - $this->getInterval();
+
+        if ($calc < 0) {
+            return $this->dispatchNoticeMessage('You can\'t go below zero');
+        }
+
+        $this->quantity = $calc;
+    }
+}
